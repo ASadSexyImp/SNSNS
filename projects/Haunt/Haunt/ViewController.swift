cc299385bf21a289328242bc6243a120d8fbbf10
ASadSexyImp <masakazuplus@gmail.com>
2019-10-01 13:37:58 +0900

    first

---
+++ projects/Haunt/Haunt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Haunt
+//
+//  Created by Masakazu Nishimura on 2019/10/01.
+//  Copyright © 2019 Masakazu Nishimura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
